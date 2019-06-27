c9303d2d4078d98147c5715e5e3a16fa9d03a5ff
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-27 15:44:09 -0700

    add select item using inquirer based on item id

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,28 +4,37 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "password",
-  database: "bamazon"
+    // Your password
+    password: "password",
+    database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
-  });
-  
-  function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      
-      connection.end();
+    start();
+});
+
+function start() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+
+        inquirer.prompt({
+            input: "input",
+            message: "What is the ID of the item you would like to purchase?",
+            name: "id"
+        })
+        .then((userIn) => {
+            console.log(res[parseInt(userIn.id) - 1]);
+        })
+
+        connection.end();
     });
 }
