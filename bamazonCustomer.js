f364cb228447122b134a1fee541ec26961fb7c84
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-27 16:07:38 -0700

    add purchase quantity functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,10 +31,36 @@ function start() {
             message: "What is the ID of the item you would like to purchase?",
             name: "id"
         })
-        .then((userIn) => {
-            console.log(res[parseInt(userIn.id) - 1]);
-        })
-
-        connection.end();
+            .then((userIn) => {
+                console.log(res[parseInt(userIn.id) - 1]);
+                buyItem(res[parseInt(userIn.id) - 1]);
+            });
     });
 }
+
+function buyItem(item) {
+    inquirer.prompt({
+        input: "input",
+        message: "How many would you like?",
+        name: "quantity"
+    })
+    .then((userIn) => {
+        connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+                {
+                    stock_quantity: item.stock_quantity - parseInt(userIn.quantity)
+                },
+                {
+                    item_id: item.item_id
+                }
+            ],
+            function() {
+                console.log(item);
+                start();
+            }
+        );
+    });
+}
+
+
